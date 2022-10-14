5f370a9bcd3e3689cff0b3c280408a529ff8e4a4
yatake <73799092+yatake-a@users.noreply.github.com>
2022-10-14 10:02:52 +0900

    Initial Commit

---
+++ QRcodeSample/QRcodeSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRcodeSampleApp.swift
+//  QRcodeSample
+//
+//  Created by 矢竹昭博 on 2022/10/14.
+//
+
+import SwiftUI
+
+@main
+struct QRcodeSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
